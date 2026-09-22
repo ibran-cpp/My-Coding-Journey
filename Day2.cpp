f2782fbeea7325bb7f292aaa997711a50031eaa3
ibran-cpp <ibranmalik317@gmail.com>
2026-09-22 09:20:43 +0530

    Add basic voting eligibility program in Day2.cpp

---
+++ Day2.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+	
+	string name;
+	int age;
+	
+	
+	cout<<"apna name likhe";
+	cin>>name;
+	
+	
+	cout<<"apni age likhe";
+	cin>>age;
+	
+	
+	if (age >= 18)
+	
+	{
+		
+		cout<<"aap vote de sakte hain";
+		
+	}
+	
+	else
+	
+	{
+		
+		cout<<"aap vote nhi de sakte";	
+		
+	}
+	
+	return 0;
+	
+	
+}
